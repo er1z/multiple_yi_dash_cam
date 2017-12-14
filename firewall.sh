e3fe714868e7197518d30c8a350c781652c390c9
eRIZ <przemyslaw.pawliczuk@h2p.pl>
2017-12-14 01:09:48 +0100

    management port

--- firewall.sh
+++ firewall.sh
@@ -13,3 +13,6 @@ ip rule add from 192.168.1.22 lookup 2
 
 iptables -t nat -A OUTPUT -d 192.168.1.11 -p tcp --dport 80 -j DNAT --to 192.168.1.254:80
 iptables -t nat -A OUTPUT -d 192.168.1.22 -p tcp --dport 80 -j DNAT --to 192.168.1.254:80
+
+iptables -t nat -A OUTPUT -d 192.168.1.11 -p tcp --dport 3333 -j DNAT --to 192.168.1.254:3333
+iptables -t nat -A OUTPUT -d 192.168.1.22 -p tcp --dport 3333 -j DNAT --to 192.168.1.254:3333
